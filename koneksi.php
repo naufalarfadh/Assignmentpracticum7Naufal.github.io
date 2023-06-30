cf0cc361049bbbf80fe3328f0e0f0053f8ec2d48
unknown <naufalarfadh@gmail.com>
2023-06-30 21:27:20 +0700

    first commit

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+$db_host = "localhost"; #simpan database host
+$db_user = "root"; #simpan username user
+$db_pass = ""; # simpan password
+$db_name = "my_blog";#simpan nama database
+
+$koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+?>
